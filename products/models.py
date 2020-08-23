e0486e8f260300584b1400a8626b5560b4b3238b
waynecrawley <enyawcrawley@gmail.com>
2020-08-23 14:24:57 +0000

    added shop bag updates

--- products/models.py
+++ products/models.py
@@ -1,13 +1,11 @@
 from django.db import models
 
-# Create your models here.
-
 
 class Category(models.Model):
 
     class Meta:
         verbose_name_plural = 'Categories'
-
+        
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -23,6 +21,7 @@ class Product(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
@@ -30,4 +29,3 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
-        
